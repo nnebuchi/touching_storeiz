db7fbd9163e541b9d9d2f136254c54c557035ff1
Debe <emmanueljoce@gmail.com>
2023-04-03 19:01:07 +0100

    fixed navbar avatar for mobile and footer

--- resources/views/layouts/main/footer.blade.php
+++ resources/views/layouts/main/footer.blade.php
@@ -1,24 +1,28 @@
 <footer>
     <section class="footer_section fixed-lg-bottom">
-      <div class=" row footer_container">
-        <div class="col-12 col-md-3 col-lg-3 offset-lg-1 col-xl-3  pt-2 pt-xl-2 pt-lg-0 px-4 px-lg-0 ">
+      <div class=" row footer_container ps-5 pe-1 pe-lg-5">
+        <div class="col-12 col-lg-3  pt-2 pt-xl-2 pt-lg-0 px-4 px-lg-0 ">
           <a class="navbar-brand cust__navbar-brand" href="#">{{env("APP_NAME")}}</a>
         </div>
   
-        <div class="navbar-nav flex-row col-12 col-md-4 col-lg-4 col-xl-4 offset-lg-0  px-4 px-lg-0   ">
+        <div class="navbar-nav flex-row col-12 col-lg-5 offset-lg-0  px-4 px-lg-0 ">
           <li class="nav-item">
-            <a class="cust_nav-link nav-link ms-lg-5" href="#">Contact Us</a>
+            <a class="cust_nav-link nav-link ms-lg-5" href="#">Contact</a>
           </li>
           <li class="nav-item">
-            <a class="cust_nav-link nav-link ms-lg-5 ms-2" href="#">Community</a>
+            <a class="cust_nav-link nav-link ms-lg-5 ms-2" href="#">Terms</a>
           </li>
           <li class="nav-item">
-            <a class="cust_nav-link nav-link ms-lg-5 ms-2" href="#">About us</a>
+            <a class="cust_nav-link nav-link ms-lg-5 ms-2" href="#">Privacy</a>
+          </li>
+          <li class="nav-item">
+            <a class="cust_nav-link nav-link ms-lg-5 ms-2" href="#">About</a>
           </li>
         </div>
   
-        <div class="col-12 col-md-4 col-lg-4 col-xl-4  pt-2 pt-xl-2  pt-lg-0 px-4 px-lg-0 ">
-          <p class="copyright">© {{env('APP_NAME')}}. <script>document.write(new Date().getFullYear());</script>. Recreating your world!</p>
+        <div class="col-12 col-lg-4  pt-2 pt-xl-2  pt-lg-0 px-4 px-lg-0 text-lg-end">
+          <p class="copyright">© {{env('APP_NAME')}}. <script>document.write(new Date().getFullYear());</script>. Recreating your world!
+          </p>
         </div>
       </div>
     </section>
--- resources/views/layouts/main/navbar.blade.php
+++ resources/views/layouts/main/navbar.blade.php
@@ -2,10 +2,31 @@
   <nav class="navbar navbar-expand-lg py-2 cust_nav fixed-top">
       <div class="container">
         <a class="navbar-brand cust__navbar-brand" href="{{route('home')}}"><img src="{{asset('assets/img/logo/logo_1.png')}}" alt="" style="width:150px; height:50px; object-fit:cover;"></a>
+        @auth
+          <div class="nav-item dropdown ms-auto me-3 d-lg-none">
+            <a class="cust_nav-link nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <span class="d-lg-inline">
+                @if(auth()->user()->is_writer && auth()->user()->cover_photo)
+                  <img src="{{asset('storage/'.auth()->user()->cover_photo)}}" height="40" class="rounded-circle" alt="Avatar">
+                @else
+                  <img src="{{asset('assets/img/avatars/avatar.png')}}" height="40" class="rounded-circle" alt="Avatar">
+                @endif
+              </span> 
+            </a>
+            <ul class="dropdown-menu me-5 me-lg-0">
+            
+              <li><a class="dropdown-item" href="route('my-stories')">My Stories</a></li>
+              <li><a class="dropdown-item" href="{{route('new-ticket')}}">Raise Ticket</a></li>
+              <li><a class="dropdown-item text-danger" href="javascript::void()" onclick="logout()">Logout</a></li>
+            </ul>
+          </div>      
+        @endauth
         
+
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
+        
         <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
           <ul class="navbar-nav cust_navbar-nav">
               <li class="nav-item dropdown">
@@ -24,12 +45,14 @@
           </ul>
           @if(Auth::check() && Auth::user()->is_writer)
             
-            <div class="nav-item dropdown ms-auto">
+            <div class="nav-item dropdown ms-auto d-none d-lg-inline">
               <a class="cust_nav-link nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                @if(Auth::user()->cover_photo == null)
-                <span><img src="{{asset('assets/img/avatars/avatar.png')}}" height="40" alt="Avatar"></span> {{Auth::user()->pen_name}}
+                @if(Auth::user()->cover_photo == null) 
+                {{Auth::user()->pen_name}}
+                <span class="d-lg-inline"><img src="{{asset('assets/img/avatars/avatar.png')}}" height="40" class="rounded-circle" alt="Avatar"></span>
                 @else
-                <span><img src="{{asset('storage/'.Auth::user()->cover_photo)}}" height="40" alt="Avatar"></span> {{Auth::user()->pen_name}}
+                {{Auth::user()->pen_name}}
+                <span class="d-lg-inline"><img src="{{asset('storage/'.Auth::user()->cover_photo)}}" height="40" class="rounded-circle" alt="Avatar"></span> 
                 @endif
               </a>
               <ul class="dropdown-menu me-5 me-lg-0">
@@ -42,9 +65,10 @@
             </div>
           @elseif(Auth::check() && !Auth::user()->is_writer)
             <a href="{{ route('become-a-writer') }}" class="cust_btn-1 ms-auto" ><strong> Become a Writer</strong></a>
-            <div class="nav-item dropdown ms-auto">
+            <div class="nav-item dropdown ms-auto d-none d-lg-inline">
               <a class="cust_nav-link nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <span><img src="{{asset('assets/img/avatars/avatar.png')}}" height="40" alt="Avatar"></span> {{Auth::user()->username}}
+                {{Auth::user()->username}}
+                <span class="d-lg-inline"><img src="{{asset('assets/img/avatars/avatar.png')}}" height="40" class="rounded-circle" alt="Avatar"></span> 
               </a>
               <ul class="dropdown-menu me-5 me-lg-0">
                
