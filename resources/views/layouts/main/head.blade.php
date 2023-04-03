49161888afc15681aa2b8a5a5b9402dd3f4a3197
Debe <emmanueljoce@gmail.com>
2023-04-03 21:57:28 +0100

    added favicon

--- resources/views/layouts/main/head.blade.php
+++ resources/views/layouts/main/head.blade.php
@@ -2,6 +2,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/img/logo/icon_1.ico') }}">
     <!-- stylesheet -->
     <link rel="stylesheet" href="{{asset('assets/css/main.css')}}">
     
